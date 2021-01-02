9014911e186f2c5b2247ea919c1fdd0e56ff6a30
wumo1016 <760478279@qq.com>
2021-01-02 11:17:06 +0800

    add(data的代理)

--- vue2/src/state.js
+++ vue2/src/state.js
@@ -1,5 +1,9 @@
-import { observe } from "./observer/index"
-import { isFunction } from "./utils"
+import {
+  observe
+} from "./observer/index"
+import {
+  isFunction
+} from "./utils"
 
 export function initState(vm) {
   const opts = vm.$options
@@ -8,8 +12,25 @@ export function initState(vm) {
   }
 }
 
+function proxy(vm, data, key) {
+  Object.defineProperty(vm, key, {
+    get() {
+      return vm[data][key]
+    },
+    set(newValue) {
+      log(123465)
+      vm[data][key] = newValue
+    }
+  })
+}
+
 function initData(vm) {
   let data = vm.$options.data
-  data = vm._data =  isFunction(data) ? data.call(vm) : data // 对data的处理，可以是对象或者函数
+  data = vm._data = isFunction(data) ? data.call(vm) : data // 对data的处理，可以是对象或者函数
+
+  for (const key in data) {
+    proxy(vm, '_data', key) // 将data的属性代理到this实例上
+  }
+
   observe(data)
 }
