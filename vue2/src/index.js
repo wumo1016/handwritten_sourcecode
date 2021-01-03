44c2f03881044c851fa40f39d1aca176a9a4484f
wumo1016 <760478279@qq.com>
2021-01-03 20:01:50 +0800

    update(生成ast语法树)

--- vue2/src/index.js
+++ vue2/src/index.js
@@ -1,8 +1,9 @@
+
 import {
   initMixin
 } from "./init"
 
-window.log = console.log
+
 
 function Vue(options) {
   this._init(options)
--- vue2/src/init.js
+++ vue2/src/init.js
@@ -1,7 +1,9 @@
 import {
+  compileToFunction
+} from "./compiler/index"
+import {
   initState
 } from "./state"
-
 export function initMixin(Vue) { // 在Vue的基础上做混合操作
   Vue.prototype._init = function (options) {
     const vm = this
@@ -11,5 +13,22 @@ export function initMixin(Vue) { // 在Vue的基础上做混合操作
     // 对数据初始化
     initState(vm)
 
+    if (options.el) {
+      vm.$mount(options.el)
+    }
+
+  }
+  Vue.prototype.$mount = function (el) {
+    const vm = this
+    const options = vm.$options
+    el = document.querySelector(el)
+
+    if (!vm.$options.render) {
+      let template = options.template
+      if (!template && el) {
+        template = el.outerHTML
+      }
+      options.render = compileToFunction(template)
+    }
   }
 }
