2385572d7d0ed02e2a74bcd0ef22aa2b7bd4b91b
wumo1016 <760478279@qq.com>
2021-02-23 16:32:14 +0800

    update(ref)

--- vue3/packages/reactivity/src/ref.ts
+++ vue3/packages/reactivity/src/ref.ts
@@ -1,4 +1,4 @@
-import { hasChanged, isObject } from "@vue/shared/src"
+import { hasChanged, isArray, isObject } from "@vue/shared/src"
 import { tigger, track } from "./effect"
 import { TiggerOpTypes, TrackOpTypes } from "./operators"
 import { reactive } from "./reactive"
@@ -18,6 +18,7 @@ function createRef(rawValue, shallow = false){
 // 如果ref传入的或设置的是对象，直接用reactive包装一下
 const convert = value => isObject(value) ? reactive(value) : value
 
+// 类的 get set 原理就是使用defineProperty
 class RefImpl {
   public _value // 表示声明了_value属性
   public __v_isRef = true
@@ -37,4 +38,26 @@ class RefImpl {
   }
 }
 
-// 类的 get set 原理就是使用defineProperty
+// 可以将一个对象的值转换成ref
+export function toRef(target, key){
+  return new ObjectRefImpl(target, key)
+}
+
+export function toRefs(object){
+  const ret = isArray(object) ? new Array(object.length) : {}
+  for (const key in object) {
+    ret[key] = toRef(object, key)
+  }
+  return ret
+}
+
+class ObjectRefImpl {
+  public __v_isRef = true
+  constructor(public target, public key){}
+  get value(){
+    return this.target[this.key]
+  }
+  set value(value){
+    this.target[this.key] = value
+  }
+}
