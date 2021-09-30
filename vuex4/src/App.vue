e19898d1c5b25d33db0f7acdfbfc269bf0760f50
wumo1016 <760478279@qq.com>
2021-09-30 11:15:24 +0800

    chore(vuex4)

--- vuex4/src/App.vue
+++ vuex4/src/App.vue
@@ -1,20 +1,22 @@
 <template>
-  <div>count - {{ count }}</div>
-  <button @click="$store.state.count++">直接+1</button>
-  <div>dounble - {{ double }}</div>
   <div>
-    <button @click="add">add</button>
-    <button @click="asyncAdd">asyncAdd</button>
+    <div>count - {{ count }}</div>
+    <button @click="$store.state.count++">直接+1</button>
+    <div>dounble - {{ double }}</div>
+    <div>
+      <button @click="add">add</button>
+      <button @click="asyncAdd">asyncAdd</button>
+    </div>
+    <h6>a模块</h6>
+    <div>{{ $store.state.aCount.count }}</div>
+    <button @click="$store.commit('aCount/add', 2)">add</button>
+    <h6>b模块</h6>
+    <div>{{ $store.state.bCount.count }}</div>
+    <button @click="$store.commit('bCount/add', 2)">add</button>
+    <h6>c模块</h6>
+    <div>{{ $store.state.aCount.cCount.count }}</div>
+    <button @click="$store.commit('aCount/cCount/add', 2)">add</button>
   </div>
-  <h6>a模块</h6>
-  <div>{{ $store.state.aCount.count }}</div>
-  <button @click="$store.commit('aCount/add', 2)">add</button>
-  <h6>b模块</h6>
-  <div>{{ $store.state.bCount.count }}</div>
-  <button @click="$store.commit('bCount/add', 2)">add</button>
-  <h6>c模块</h6>
-  <div>{{ $store.state.aCount.cCount.count }}</div>
-  <button @click="$store.commit('aCount/cCount/add', 2)">add</button>
 </template>
 
 <script>
