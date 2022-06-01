33990163bc0c6d5d3341700fc905d8ed706e835e
wumo1016 <760478279@qq.com>
2022-06-01 22:12:54 +0800

    feat:reactive添加标记

--- vue3-2/packages/reactivity/src/reactive.ts
+++ vue3-2/packages/reactivity/src/reactive.ts
@@ -2,18 +2,25 @@ import { isObject } from '@vue/shared'
 
 const reactiveMap = new WeakMap()
 
+const enum ReactiveFlags {
+  IS_REACTIVE = '__v_isReactive'
+}
+
 /**
  * @Author: wyb
  * @Descripttion:
  * @param {*} target
  */
 export function reactive(target) {
-  if (!isObject(target)) return target
+  // 不是对象 或者 已经被代理过了
+  if (!isObject(target) || target[ReactiveFlags.IS_REACTIVE]) return target
+
   // 如果代理过 直接返回代理结果
   if (reactiveMap.has(target)) return reactiveMap.get(target)
 
   const proxy = new Proxy(target, {
     get(target, key, receiver) {
+      if (key === ReactiveFlags.IS_REACTIVE) return true // 代理过
       return Reflect.get(target, key, receiver)
     },
     set(target, key, value, receiver) {
