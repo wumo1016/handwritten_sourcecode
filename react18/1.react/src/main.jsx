7d57e944d2d4d74b09668a93827fe6eedbee013c
wumo1016 <760478279@qq.com>
2022-10-20 21:12:05 +0800

    init

---
+++ react18/1.react/src/main.jsx
@@ -0,0 +1,28 @@
+const element = (
+  <h1>
+    <span>hello</span>
+    <span style={{ color: 'red' }}>world</span>
+  </h1>
+)
+
+console.log(element)
+
+// import { createRoot } from 'react-dom/client'
+// function FunctionComponent() {
+//   return (
+//     <h1
+//       onClick={() => console.log(`父冒泡`)}
+//       onClickCapture={() => console.log(`父捕获`)}
+//     >
+//       <span
+//         onClick={() => console.log(`子冒泡`)}
+//         onClickCapture={() => console.log(`子捕获`)}
+//       >
+//         world
+//       </span>
+//     </h1>
+//   )
+// }
+// const element = <FunctionComponent />
+// const root = createRoot(document.getElementById('root'))
+// root.render(element)
---
+++ react18/1.react/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const resolve = (p) => path.posix.resolve(p)
+
+export default defineConfig({
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      react: path.posix.resolve('src/react')
+      // 'react-dom': path.posix.resolve('src/react-dom'), //
+      // 'react-dom-bindings': path.posix.resolve('src/react-dom-bindings'),
+      // 'react-reconciler': path.posix.resolve('src/react-reconciler'),
+      // scheduler: path.posix.resolve('src/scheduler'),
+      // shared: path.posix.resolve('src/shared')
+    }
+  },
+  plugins: [react()],
+  optimizeDeps: {
+    force: true
+  }
+})
