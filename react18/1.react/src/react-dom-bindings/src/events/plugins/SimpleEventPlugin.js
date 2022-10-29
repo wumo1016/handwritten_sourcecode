b545c3b635dd10c8192b213c95ba289f4a942387
wumo1016 <760478279@qq.com>
2022-10-29 21:02:47 +0800

    chore: 事件

--- react18/1.react/src/react-dom-bindings/src/events/plugins/SimpleEventPlugin.js
+++ react18/1.react/src/react-dom-bindings/src/events/plugins/SimpleEventPlugin.js
@@ -4,6 +4,7 @@ import {
 } from '../DOMEventProperties'
 import { IS_CAPTURE_PHASE } from '../EventSystemFlags'
 import { accumulateSinglePhaseListeners } from '../DOMPluginEventSystem'
+import { SyntheticMouseEvent } from '../SyntheticEvent'
 
 /**
  * @Author: wyb
@@ -18,18 +19,40 @@ function extractEvents(
   eventSystemFlags,
   targetContainer
 ) {
+  // 合成事件的构建函数
+  let SyntheticEventCtor
+  switch (domEventName) {
+    case 'click':
+      SyntheticEventCtor = SyntheticMouseEvent
+      break
+    default:
+      break
+  }
   // 获取 原生事件名 对应的 react 事件名
   const reactName = topLevelEventsToReactNames.get(domEventName)
   // 是否是捕获
   const isCapturePhase = (eventSystemFlags & IS_CAPTURE_PHASE) !== 0
-  // 获取事件队列
+  // 获取事件队列 先添加的都是子元素的事件
   const listeners = accumulateSinglePhaseListeners(
     targetInst,
     reactName,
     nativeEvent.type,
     isCapturePhase
   )
-  console.log(eventSystemFlags, listeners)
+  if (listeners.length > 0) {
+    // 创建合成事件实例
+    const event = new SyntheticEventCtor(
+      reactName,
+      domEventName,
+      null,
+      nativeEvent,
+      nativeEventTarget
+    )
+    dispatchQueue.push({
+      event, // 合成事件实例
+      listeners // 监听函数数组
+    })
+  }
 }
 
 export { registerSimpleEvents as registerEvents, extractEvents }
