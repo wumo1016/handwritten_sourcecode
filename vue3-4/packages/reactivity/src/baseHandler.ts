4b10663d45647aa8e6a5ea3f5b1182e33c64f4b0
wumo1016 <760478279@qq.com>
2024-04-27 18:49:33 +0800

    feat: reactive深层代理

--- vue3-4/packages/reactivity/src/baseHandler.ts
+++ vue3-4/packages/reactivity/src/baseHandler.ts
@@ -2,9 +2,11 @@
  * @Description: proxy处理方法
  * @Author: wyb
  * @LastEditors: wyb
- * @LastEditTime: 2024-04-27 12:02:30
+ * @LastEditTime: 2024-04-27 18:47:53
  */
+import { isObject } from '@vue/shared'
 import { track, trigger } from './reactiveEffect'
+import { reactive } from './reactive'
 
 /*
  * @Description:
@@ -28,7 +30,13 @@ export const mutableHandlers: ProxyHandler<object> = {
     track(target, key)
 
     // 使用 Reflect 处理 this 问题
-    return Reflect.get(target, key, recevier)
+    const res = Reflect.get(target, key, recevier)
+    if (isObject(res)) {
+      // 当取的值也是对象的时候，我需要对这个对象在进行代理，递归代理
+      return reactive(res)
+    }
+
+    return res
   },
   set(target, key, value, recevier) {
     const oldValue = target[key]
